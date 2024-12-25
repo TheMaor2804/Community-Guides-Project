bfa2eec879f681ae598a4b6402cca3623f7e4736
TheMaor2804 <mhgameing@Gmail.com>
2024-12-25 15:42:09 +0200

    add CORS and logging middleware to enhance request handling

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const corsMiddleware = require('./middlewares/cors');
 const { handleError } = require('./utils/handleErrors');
+const loggerMiddleware = require('./logger/loggerService');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(corsMiddleware);
 app.use(express.json({ limit: '10mb' }));
+
+app.use(loggerMiddleware());
 
 app.use(express.static('public'));
 
