bed7acb9b5956100c2214c23086f17a48a6e0cab
TheMaor2804 <mhgameing@Gmail.com>
2024-12-30 17:54:27 +0200

    feat: enhance LeftNavbar with logo navigation and improved layout

--- frontend/src/layout/header/left-navigation/LeftNavbar.jsx
+++ frontend/src/layout/header/left-navigation/LeftNavbar.jsx
@@ -1,18 +1,22 @@
-import { Box, Grid2, IconButton } from '@mui/material'
+import { Box, IconButton } from '@mui/material'
 import React from 'react'
 import NavBarItem from '../../../routes/components/NavbarItem'
 import ROUTES from '../../../routes/routesModel'
 import { useCurrentUser } from '../../../users/providers/UserProvider'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function LeftNavbar() {
 
     const { user } = useCurrentUser();
+    const navigate = useNavigate();
 
     return (
-        <Box sx={{ width: "33.33333%" }}>
+        <Box sx={{ display: 'block', width: "33.33333%" }}>
             {/* <NavBarItem label={"Guides"} to={ROUTES.ROOT} /> */}
+            <IconButton>
+                <img src="/images/logo.jpg" alt="logo" style={{ width: "50px", height: "50px" }} onClick={() => navigate(ROUTES.ROOT)} />
+            </IconButton>
             {user && user.isAdmin && <NavBarItem label={"CRM"} to={ROUTES.CRM} />}
             {user && (user.isMod || user.isAdmin) && <NavBarItem label={"Mod Page"} to={ROUTES.MOD_PAGE} />}
         </Box>
